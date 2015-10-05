d20350b87f2271f8ee6061c8f6d41cc7a594c1ac
IMRambo <imrambo@udel.edu>
2015-10-04 20:04:11 -0400

    script cleanup

--- get_nucleotide_ncbi.py
+++ get_nucleotide_ncbi.py
@@ -2,12 +2,13 @@
 #Ian Rambo
 #BINF868
 #University of Delaware
-#Last updated: September 23, 2015
+#Last updated: October 4, 2015
 #==============================================================================
 '''
 GOAL:
 Download nucleotide sequences for bacterial DNA methyltransferase genes.
 METHOD:
+Download nucleotide sequences from NCBI ENTREZ with urllib.
 Parse HTML source code from NCBI using regular expressions. 
 '''
 #==============================================================================
@@ -18,9 +19,9 @@ import urllib.parse
 #==============================================================================
 #Functions
 #------------------------------------------------------------------------------
-def get_gene_gi_HTML(accession, start, end) :
+def get_gene_gi(accession, start, end) :
     '''
-    Parse gene nucleotide GI numbers from NCBI HTML using gene Accession numbers. 
+    Parse gene nucleotide GI numbers from NCBI using gene Accession numbers. 
     '''
     geneGI = []
     assert len(accession) == len(start) == len(end)
@@ -46,6 +47,7 @@ def efetch_gene_nucleotide(GI, start, end, batch_size, filename) :
     Download gene nucleotide sequences in FASTA format using the NCBI
     efetch eutil. 
     '''
+    #Entrez.email = "imrambo@udel.edu"
     print('\nDownloading gene sequences...\n')
     outputHandle = open('%s' % filename, 'w')
     assert len(GI) == len(start) == len(end)
@@ -56,19 +58,26 @@ def efetch_gene_nucleotide(GI, start, end, batch_size, filename) :
                   'rettype':'fasta','retmax':str(batch_size)}
         data = urllib.parse.urlencode(values)
         data = data.encode('utf-8')
+        Headers = {}
+        #Firefox 40.1
+        Headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
         #Request the URL with the variables
-        req = urllib.request.Request(url, data)
+        req = urllib.request.Request(url, data, headers = Headers)
         #Response - visit the URL
         resp = urllib.request.urlopen(req)
         respData = resp.read().decode('utf-8')
         respData = str(respData).replace('\n\n', '\n')
         outputHandle.write(respData)
+    outputHandle.close()
+    return
 #------------------------------------------------------------------------------
 def efetch_genome_nucleotide(GI, batch_size, filename) :
     '''
     Download genomic nucleotide sequences in FASTA format using the NCBI
-    efetch eutil. 
+    efetch eutil via urllib.
     '''
+    #Import Entrez to tell NCBI who you are with Entrez.email
+    Entrez.email = 'imrambo@udel.edu'
     print('\nDownloading genome sequences...\n')
     outputHandle = open('%s' % filename, 'w')
     for i in range(0, len(GI)) :
@@ -84,7 +93,80 @@ def efetch_genome_nucleotide(GI, batch_size, filename) :
             respData = resp.read().decode('utf-8')
             respData = str(respData).replace('\n\n', '\n')
             outputHandle.write(respData)
+    outputHandle.close()
+    return
+#------------------------------------------------------------------------------
+def fasta_write_test(fasta) :
+    '''
+    Test if FASTA output file wrote correctly. 
+    '''
+    with open(fasta, 'r') as fa :
+        lines = fa.readlines()
+        if lines[0].startswith('>') and lines[0].endswith('\n') and not '>' in lines[-1] :
+            print('FASTA file written successfully.')
+        else :
+            print('FASTA file not written successfully.')
+    fa.close()
+    return
 #==============================================================================
+#START
+#==============================================================================
+#BACTERIAL DNA METHYLTRANSFERASES
+#==============================================================================
+'''
+#Parse txt file from NCBI gene search
+os.chdir('/Users/imrambo/Documents/BINF868/')
+accession = []
+start = []
+stop = []
+#txt file containing gene search results, downloaded from NCBI
+geneInfo = 'dcm_genome_result.txt'
+
+
+nonTerms = ['hypothetical protein', 'RNA methyltransferase', 'putative']
+with open(geneInfo, 'r') as gres :
+    for record in gres :
+        record = record.rstrip()
+        recordL = record.split('\t')
+        if not any(x in recordL[7] for x in nonTerms) and len(recordL) == 16 :
+            accession.append(recordL[11])
+            start.append(recordL[12])
+            stop.append(recordL[13])
+           
+        
+gres.close()
+
+batch_size = 500
+mtgi = get_gene_gi(accession, start, stop)
+
+outputHandle = open('dcm_genome.fa', 'w')
+url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi'
+assert len(mtgi) == len(start) == len(stop)
+print('Downloading nucleotide sequences...')
+for i in range(0, len(mtgi)) :
+    values = {'db':'nuccore','id':mtgi[i],
+              'seq_start':start[i], 'seq_stop':stop[i],
+              'rettype':'fasta','retmax':str(batch_size)}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    Headers = {}
+    #Firefox 40.1
+    #Headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+    #Request the URL with the variables
+    req = urllib.request.Request(url, data)#, headers = Headers)
+    #Response - visit the URL
+    resp = urllib.request.urlopen(req)
+    respData = resp.read().decode('utf-8')
+    respData = str(respData).replace('\n\n', '\n')
+    outputHandle.write(respData)
+
+outputHandle.close()
+
+print('Download successful.')
+'''
+#==============================================================================
+#HTML regex parsing method. Caveat - I don't know how to download
+#more than 20 records at a time. 
 '''
 #Search NCBI for bacterial methyltransferase
 #gene IDs, Accession numbers, strands, and loci.
@@ -117,44 +199,16 @@ for e in location :
     locusEnd.extend(re.findall(r'\.\.(\d+)\,?', e))
 #------------------------------------------------------------------------------    
 #List of GI numbers for dam MTase nucleotide records
-mtaseGI = get_gene_gi_HTML(accession, locusStart, locusEnd)
+mtaseGI = get_gene_gi(accession, locusStart, locusEnd)
 
 os.chdir('/Users/imrambo/Documents/BINF868/')
 #Fetch nucleotide sequences for dam MTases
 efetch_gene_nucleotide(mtaseGI, locusStart, locusEnd, 5, 'dam.fa')
 '''
 #==============================================================================
-#Bacterial target genomes
-#Define taxon of interest for NCBI search
-taxon = 'Desulfuromonadales'
-
-url = 'http://www.ncbi.nlm.nih.gov/nuccore/?term=' + taxon + '[Orgn]+AND+%28biomol_genomic[PROP]+AND+refseq[filter]%29'
-#Search results for taxon
-URL = urllib.request.urlopen(url)
-HTML = URL.read()
-#Parse nucleotide GI numbers
-genomeGI = re.findall(r'GI:</dt>\s*<dd>(\d+)</dd>', str(HTML))
-
-print('\nParsing %s genome GIs...\n' % taxon)
-efetch_genome_nucleotide(genomeGI, 5, '%s_genomes.fa' % taxon)
-
-
-
-
+#TARGET GENOMES
 #==============================================================================
-#Parse txt file from NCBI gene search
-accession = []
-start = []
-stop = []
-with open('mtase_gene_results_test.txt', 'r') as gres :
-    for record in gres :
-        record = record.rstrip()
-        recordl = record.split()
-        accession.append(recordl[0])
-        start.append(recordl[1])
-        stop.append(recordl[2])
-        
-gres.close()
+os.chdir('/Users/imrambo/Documents/BINF868')
 
-print(accession)
-        
+
+
